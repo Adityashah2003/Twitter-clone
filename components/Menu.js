987d4f5e2f36f9cf3f18f04b22e9d5cb7f93d4f5
Adityashah2003 <adigshah14@gmail.com>
2023-07-20 01:12:40 +0530

    sidebar completed

---
+++ components/Menu.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Menu({text,Icon,active}) {
+  return (
+    <div className='hoverEffect flex items-center text-grey-700 justify-center xl:justify-start text-lg space-x-3'>
+        <Icon className="h-7"/>
+        <span className={`${active && "font-bold"} hidden xl:inline`}>{text}</span>
+    </div>
+  )
+}
