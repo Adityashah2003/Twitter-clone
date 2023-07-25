eb8e0c2aa2ed4e5d61cf81c607bcb87a06476906
Adityashah2003 <adigshah14@gmail.com>
2023-07-25 18:56:10 +0530

    added animations

--- components/Post.js
+++ components/Post.js
@@ -3,9 +3,10 @@ import { HeartIcon } from '@heroicons/react/solid'
 import React, { useEffect, useState } from 'react'
 import Moment from 'react-moment';
 import { collection, onSnapshot, doc, setDoc, deleteDoc } from 'firebase/firestore';
-import { db } from '../firebase';
-import { useSession } from 'next-auth/react';
+import { db,storage } from '../firebase';
+import { signIn, useSession } from 'next-auth/react';
 import {HeartIcon as HeartIconFilled} from "@heroicons/react/solid"
+import { deleteObject, ref } from 'firebase/storage';
 
 export default function Post({ post }) {
   const {data:session} = useSession();
@@ -23,16 +24,29 @@ useEffect(()=>{
 },[likes])
 
   async function likePost(){
-    if(hasLiked){
-      await deleteDoc(doc(db,"posts",post.id,"likes",session?.user.uid))
+    if(session){
+      if(hasLiked){
+        await deleteDoc(doc(db,"posts",post.id,"likes",session?.user.uid))
+      }
+      else{
+        await setDoc(doc(db,"posts",post.id,"likes",session?.user.uid),{
+          username: session.user.username
+        })
+      }
     }
     else{
-      await setDoc(doc(db,"posts",post.id,"likes",session?.user.uid),{
-        username: session.user.username
-      })
+      signIn()
+    }
+    
+  }
+  async function deletePost(){
+    if(window.confirm("Are you sure")){
+      deleteDoc(doc(db,"posts",post.id))
+      if(post.data().image){
+        deleteObject(ref(storage, `posts/${post.id}/image`));
+      }
     }
   }
-
 
 
   return (
@@ -63,7 +77,10 @@ useEffect(()=>{
         {/* icons */}
         <div className='flex justify-between text-gray-500 p-2'>
           <ChatIcon className='h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100' />
-          <TrashIcon className='h-9 w-9 hoverEffect p-2 hover:text-red-500 hover:bg-red-100' />
+          {session?.user.uid === post?.data().id && (
+          <TrashIcon onClick={deletePost} className='h-9 w-9 hoverEffect p-2 hover:text-red-500 hover:bg-red-100' />
+          )}          
+
           <div className='flex'>
             {hasLiked?(
               <HeartIconFilled onClick={likePost}className='h-9 w-9 hoverEffect p-2 text-red-500 hover:bg-red-100' />
--- components/Widgets.js
+++ components/Widgets.js
@@ -1,6 +1,7 @@
 import { SearchIcon } from "@heroicons/react/outline";
 import News from "./News";
 import { useState } from "react";
+import { AnimatePresence,motion } from "framer-motion";
 
 export default function Widgets({newsRes , randUsers}) {
   const [artnum,setartnum]=useState(3);
@@ -17,17 +18,25 @@ export default function Widgets({newsRes , randUsers}) {
 
         <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[90%] xl:w-[95%]">
           <h4 className="font-bold text-xl px-4">Whats happening</h4>
-          {newsRes.slice(0,artnum).map((article) => (
-            <News key={article.title} article = {article}/>
-          ))}
+          <AnimatePresence >
+            {newsRes.slice(0,artnum).map((article) => (
+              <motion.div key={article.title} initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}} transition={{duration:1}}>
+                <News key={article.title} article = {article}/>
+              </motion.div>
+            ))}
+          </AnimatePresence>
+          
           <button onClick={()=>setartnum(artnum+3)} className="text-blue-300 pl-4 pb-3 hover:text-blue-400">Show more</button>
         </div>
 
         <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[90%] xl:w-[95%] sticky top-16">
           <h4 className="font-bold text-xl px-4">Who to follow</h4>
-          
+
+          <AnimatePresence>
           {randUsers.slice(0,randNum).map((randomuser) => (
-            <div key={randomuser.login.username} className="flex items-center px-4 py-2 cursor-pointer hover:bg-gray-200 ">
+            <motion.div key={randomuser.login.username} initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}} transition={{duration:1}}>          
+
+            <div key={randomuser.login.username} className="flex items-center px-4 py-2 cursor-pointer hover:bg-gray-200 hover:bg-gray-200 transition duration-400 ease-out">
               <img src={randomuser.picture.thumbnail} alt="img"  width="50" className="rounded-full ml-4"/>
               <div className="truncate ml-4 leading-5">
                 <h4 className="font-bold hover:underline text-[14px] truncate">{randomuser.login.username}</h4>
@@ -35,7 +44,9 @@ export default function Widgets({newsRes , randUsers}) {
               </div>
             <button className="ml-auto bg-black text-white rounded-full text-sm px-3.5 py-1.5 font-bold">Follow</button>
             </div>
+            </motion.div>
           ))}
+          </AnimatePresence>
           <button onClick={()=>setrandNum(randNum+3)} className="text-blue-300 pl-4 pb-3 hover:text-blue-400">Show more</button>
 
         </div>
