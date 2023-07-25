5af0c7db5321865693fbdfe701d99cec36ec1ed4
Adityashah2003 <adigshah14@gmail.com>
2023-07-25 17:42:15 +0530

    added like functionality

--- components/Post.js
+++ components/Post.js
@@ -1,9 +1,40 @@
 import { ChartBarIcon, ChatIcon, DotsHorizontalIcon, ShareIcon, TrashIcon } from '@heroicons/react/outline'
 import { HeartIcon } from '@heroicons/react/solid'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Moment from 'react-moment';
+import { collection, onSnapshot, doc, setDoc, deleteDoc } from 'firebase/firestore';
+import { db } from '../firebase';
+import { useSession } from 'next-auth/react';
+import {HeartIcon as HeartIconFilled} from "@heroicons/react/solid"
 
 export default function Post({ post }) {
+  const {data:session} = useSession();
+  const [likes,setLikes] = useState([]);
+  const [hasLiked,setHasLiked]=useState(false); 
+
+  useEffect(()=>{
+    const unsubscribe = onSnapshot(
+      collection(db,"posts",post.id,"likes"),(snapshot)=> setLikes(snapshot.docs)
+    );
+  },[db]);
+
+useEffect(()=>{
+  setHasLiked(likes.findIndex((like)=>like.id === session?.user.uid)!==-1)
+},[likes])
+
+  async function likePost(){
+    if(hasLiked){
+      await deleteDoc(doc(db,"posts",post.id,"likes",session?.user.uid))
+    }
+    else{
+      await setDoc(doc(db,"posts",post.id,"likes",session?.user.uid),{
+        username: session.user.username
+      })
+    }
+  }
+
+
+
   return (
     <div className='flex p-3 cursor-pointer border-b border-gray-200'>
       {/* image */}
@@ -33,7 +64,17 @@ export default function Post({ post }) {
         <div className='flex justify-between text-gray-500 p-2'>
           <ChatIcon className='h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100' />
           <TrashIcon className='h-9 w-9 hoverEffect p-2 hover:text-red-500 hover:bg-red-100' />
-          <HeartIcon className='h-9 w-9 hoverEffect p-2 hover:text-red-500 hover:bg-red-100' />
+          <div className='flex'>
+            {hasLiked?(
+              <HeartIconFilled onClick={likePost}className='h-9 w-9 hoverEffect p-2 text-red-500 hover:bg-red-100' />
+            ):(
+              <HeartIcon onClick={likePost}className='h-9 w-9 hoverEffect p-2 hover:text-red-500 hover:bg-red-100' />
+            )}
+            {likes.length > 0 && (
+              <span className={`${hasLiked && "text-red-600 mt-1.5"} text-sm- select-none `} >{likes.length}</span>
+            )}
+          </div>
+          
           <ShareIcon className='h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100' />
           <ChartBarIcon className='h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100' />
         </div>
